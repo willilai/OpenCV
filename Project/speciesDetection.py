ec7e23a04d8f1457a7f4e12856c10e5fe5adb16c
willilai <william.lai.23@bishops.com>
2022-09-25 15:41:57 -0700

    organized code into functions

--- Project/speciesDetection.py
+++ Project/speciesDetection.py
@@ -16,49 +16,74 @@ def plot_histogram(image, title, mask = None):
         plt.plot(hist, color = color)
         plt.xlim([0, 256])
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required = True, help = "Path to the Image")
-args = vars(ap.parse_args())
+    plt.show()
+    cv2.waitKey(0)
 
-image = cv2.imread(args["image"])
-cv2.imshow("Original", image)
+def imageManipulation(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (21, 21), 0)
+    sobelX = cv2.Sobel(blurred, cv2.CV_64F, 1, 0)
+    sobelY = cv2.Sobel(blurred, cv2.CV_64F, 0, 1)
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(gray, (21, 21), 0)
-sobelX = cv2.Sobel(blurred, cv2.CV_64F, 1, 0)
-sobelY = cv2.Sobel(blurred, cv2.CV_64F, 0, 1)
+    sobelX = np.uint8(np.absolute(sobelX))
+    sobelY = np.uint8(np.absolute(sobelY))
 
-sobelX = np.uint8(np.absolute(sobelX))
-sobelY = np.uint8(np.absolute(sobelY))
+    sobelCombined = cv2.bitwise_or(sobelX, sobelY)
 
-sobelCombined = cv2.bitwise_or(sobelX, sobelY)
+    edged = cv2.Canny(sobelCombined, 10, 70)
+    cv2.imshow("Edges", edged)
 
-edged = cv2.Canny(sobelCombined, 10, 70)
-cv2.imshow("Edges", edged)
+    return edged
 
- #morphology ops
-kernel1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (23,23))
-kernel2 = np.ones((15,15), np.uint8)
-#eliplical erode to get rid of some of the noise
-openFrame = cv2.dilate(edged, kernel2, iterations = 1)
-erodedFrame = cv2.erode(openFrame, kernel1, iterations = 1)
-cv2.imshow("morhp ops", erodedFrame)
+def morphManip(edged):
+     #morphology ops
+    kernel1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (23,23))
+    kernel2 = np.ones((15,15), np.uint8)
+    #eliplical erode to get rid of some of the noise
+    openFrame = cv2.dilate(edged, kernel2, iterations = 1)
+    erodedFrame = cv2.erode(openFrame, kernel1, iterations = 1)
+    cv2.imshow("morhp ops", erodedFrame)
 
-(cnts, _) = cv2.findContours(erodedFrame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    return erodedFrame
 
-whale = image.copy()
-c = max(cnts, key=cv2.contourArea)
-cv2.drawContours(whale, c, -1, (0, 255, 0), 2)
-cv2.imshow("Whale", whale)
+def contours(erodedFrame, image):
+    (cnts, _) = cv2.findContours(erodedFrame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-mask = np.zeros(image.shape[:2], dtype = "uint8")
-cv2.fillConvexPoly(mask, c, (255, 255, 255))
-cv2.imshow("Mask", mask)
-cv2.imshow("Masked Whale", cv2.bitwise_and(image, image, mask = mask))
+    whale = image.copy()
+    c = max(cnts, key=cv2.contourArea)
+    cv2.drawContours(whale, c, -1, (0, 255, 0), 2)
+    cv2.imshow("Whale", whale)
 
-plot_histogram(image, "Histogram for Masked Image", mask = mask)
+    return c
+
+def applyMask(c, image):
+    mask = np.zeros(image.shape[:2], dtype = "uint8")
+    cv2.fillConvexPoly(mask, c, (255, 255, 255))
+    cv2.imshow("Mask", mask)
+    cv2.imshow("Masked Whale", cv2.bitwise_and(image, image, mask = mask))
+
+    return mask
+
+def histofOImage():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required = True, help = "Path to the Image")
+    args = vars(ap.parse_args())
+
+    image = cv2.imread(args["image"])
+    cv2.imshow("Original", image)
+
+    edged = imageManipulation(image)
+    erodedFrame = morphManip(edged)
+    c = contours(erodedFrame, image)
+    mask = applyMask(c, image)
+
+    plot_histogram(image, "Histogram for Masked Image", mask = mask)
+
+def histofNImage():
+    
+def main():
+    histofOImage()
+    histofNImage()
 
 
-plt.show()
-
-cv2.waitKey(0)
+main()
