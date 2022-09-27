e0bd9f298a6690c94224608d4015152c7d0bc7a3
willilai <william.lai.23@bishops.com>
2022-09-27 00:19:34 -0700

    used new images

--- Project/speciesDetection.py
+++ Project/speciesDetection.py
@@ -1,23 +1,8 @@
 from matplotlib import pyplot as plt
+from statistics import mean
 import numpy as np
 import argparse
 import cv2
-
-def plot_histogram(image, title, mask = None):
-    chans = cv2.split(image)
-    colors = ("b", "g", "r")
-    plt.figure()
-    plt.title(title)
-    plt.xlabel("Bins")
-    plt.ylabel("# of Pixels")
-
-    for (chan, color) in zip (chans, colors):
-        hist = cv2.calcHist([chan], [0], mask, [256], [0, 256])
-        plt.plot(hist, color = color)
-        plt.xlim([0, 256])
-
-    plt.show()
-    cv2.waitKey(0)
 
 def imageManipulation(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -60,27 +45,77 @@ def applyMask(c, image):
     mask = np.zeros(image.shape[:2], dtype = "uint8")
     cv2.fillConvexPoly(mask, c, (255, 255, 255))
     cv2.imshow("Mask", mask)
-    cv2.imshow("Masked Whale", cv2.bitwise_and(image, image, mask = mask))
+    # maskWhale = cv2.bitwise_and(image, image, mask = mask)
+    # cv2.imshow("Masked Whale", maskWhale)
 
     return mask
 
-def histofOImage():
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--image", required = True, help = "Path to the Image")
-    args = vars(ap.parse_args())
+def plot_histogram(image, title, mask = None):
+    chans = cv2.split(image)
+    colors = ("b", "g", "r")
+    plt.figure()
+    plt.title(title)
+    plt.xlabel("Bins")
+    plt.ylabel("# of Pixels")
 
-    image = cv2.imread(args["image"])
-    cv2.imshow("Original", image)
+    for (chan, color) in zip (chans, colors):
+        hist = cv2.calcHist([chan], [0], mask, [256], [0, 256])
+        plt.plot(hist, color = color)
+        plt.xlim([0, 256])
 
-    edged = imageManipulation(image)
+    plt.show()
+    cv2.waitKey(0)
+
+def blue():
+    blue = cv2.imread("images/blue1.png")
+    cv2.imshow("Blue Whale", blue)
+    edged = imageManipulation(blue)
     erodedFrame = morphManip(edged)
-    c = contours(erodedFrame, image)
-    mask = applyMask(c, image)
+    c = contours(erodedFrame, blue)
+    mask = applyMask(c, blue)
+    maskWhale = cv2.bitwise_and(blue, blue, mask = mask)
+    cv2.imshow("Masked Whale", maskWhale)
 
-    plot_histogram(image, "Histogram for Masked Image", mask = mask)
+    plot_histogram(blue, "Histogram for Masked Image", mask = mask)
+
+def fin():
+    fin = cv2.imread("images/fin1.jpg")
+    cv2.imshow("Fin Whale", fin)
+    edged = imageManipulation(fin)
+    erodedFrame = morphManip(edged)
+    c = contours(erodedFrame, fin)
+    mask = applyMask(c, fin)
+
+    plot_histogram(fin, "Histogram for Masked Image", mask = mask)
+
+def humpback():
+    humpback = cv2.imread("images/humpback1.jpg")
+    cv2.imshow("Humpback Whale", humpback)
+    edged = imageManipulation(humpback)
+    erodedFrame = morphManip(edged)
+    c = contours(erodedFrame, humpback)
+    mask = applyMask(c, humpback)
+
+    plot_histogram(humpback, "Histogram for Masked Image", mask = mask)
+
+def beluga():
+    beluga = cv2.imread("images/beluga1.jpg")
+    cv2.imshow("Beluga Whale", beluga)
+    edged = imageManipulation(beluga)
+    erodedFrame = morphManip(edged)
+    c = contours(erodedFrame, beluga)
+    mask, maskedWhale = applyMask(c, beluga)
+
+    plot_histogram(beluga, "Histogram for Masked Image", mask = mask)
+
+def histofOImage():
+    blue()
+    #fin()
+    #humpback()
+    #beluga()
 
 def histofNImage():
-    
+    pass
 def main():
     histofOImage()
     histofNImage()
