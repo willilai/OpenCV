6aa351915a0296ac27cb23b865d2ba8cb918a4ec
willilai <william.lai.23@bishops.com>
2022-09-27 00:32:18 -0700

    collects all the histograms and saves them in a list of list

--- Project/speciesDetection.py
+++ Project/speciesDetection.py
@@ -16,7 +16,7 @@ def imageManipulation(image):
     sobelCombined = cv2.bitwise_or(sobelX, sobelY)
 
     edged = cv2.Canny(sobelCombined, 10, 70)
-    cv2.imshow("Edges", edged)
+    # cv2.imshow("Edges", edged)
 
     return edged
 
@@ -27,7 +27,7 @@ def morphManip(edged):
     #eliplical erode to get rid of some of the noise
     openFrame = cv2.dilate(edged, kernel2, iterations = 1)
     erodedFrame = cv2.erode(openFrame, kernel1, iterations = 1)
-    cv2.imshow("morhp ops", erodedFrame)
+    # cv2.imshow("morhp ops", erodedFrame)
 
     return erodedFrame
 
@@ -37,20 +37,21 @@ def contours(erodedFrame, image):
     whale = image.copy()
     c = max(cnts, key=cv2.contourArea)
     cv2.drawContours(whale, c, -1, (0, 255, 0), 2)
-    cv2.imshow("Whale", whale)
+    # cv2.imshow("Whale", whale)
 
     return c
 
 def applyMask(c, image):
     mask = np.zeros(image.shape[:2], dtype = "uint8")
     cv2.fillConvexPoly(mask, c, (255, 255, 255))
-    cv2.imshow("Mask", mask)
-    # maskWhale = cv2.bitwise_and(image, image, mask = mask)
-    # cv2.imshow("Masked Whale", maskWhale)
+    # cv2.imshow("Mask", mask)
+    maskWhale = cv2.bitwise_and(image, image, mask = mask)
+    cv2.imshow("Masked Whale", maskWhale)
 
     return mask
 
 def plot_histogram(image, title, mask = None):
+    hists = []
     chans = cv2.split(image)
     colors = ("b", "g", "r")
     plt.figure()
@@ -60,65 +61,77 @@ def plot_histogram(image, title, mask = None):
 
     for (chan, color) in zip (chans, colors):
         hist = cv2.calcHist([chan], [0], mask, [256], [0, 256])
+        hists.append(hist)
         plt.plot(hist, color = color)
         plt.xlim([0, 256])
 
     plt.show()
-    cv2.waitKey(0)
+
+    return hists
 
 def blue():
     blue = cv2.imread("images/blue1.png")
-    cv2.imshow("Blue Whale", blue)
+    # cv2.imshow("Blue Whale", blue)
     edged = imageManipulation(blue)
     erodedFrame = morphManip(edged)
     c = contours(erodedFrame, blue)
     mask = applyMask(c, blue)
-    maskWhale = cv2.bitwise_and(blue, blue, mask = mask)
-    cv2.imshow("Masked Whale", maskWhale)
 
-    plot_histogram(blue, "Histogram for Masked Image", mask = mask)
+    hists = plot_histogram(blue, "Histogram for Masked Image", mask = mask)
+
+    return hists
 
 def fin():
-    fin = cv2.imread("images/fin1.jpg")
-    cv2.imshow("Fin Whale", fin)
+    fin = cv2.imread("images/fin1.png")
+    # cv2.imshow("Fin Whale", fin)
     edged = imageManipulation(fin)
     erodedFrame = morphManip(edged)
     c = contours(erodedFrame, fin)
     mask = applyMask(c, fin)
 
-    plot_histogram(fin, "Histogram for Masked Image", mask = mask)
+    hists = plot_histogram(fin, "Histogram for Masked Image", mask = mask)
+
+    return hists
 
 def humpback():
-    humpback = cv2.imread("images/humpback1.jpg")
-    cv2.imshow("Humpback Whale", humpback)
+    humpback = cv2.imread("images/humpback1.png")
+    # cv2.imshow("Humpback Whale", humpback)
     edged = imageManipulation(humpback)
     erodedFrame = morphManip(edged)
     c = contours(erodedFrame, humpback)
     mask = applyMask(c, humpback)
 
-    plot_histogram(humpback, "Histogram for Masked Image", mask = mask)
+    hists = plot_histogram(humpback, "Histogram for Masked Image", mask = mask)
+
+    return hists
 
 def beluga():
-    beluga = cv2.imread("images/beluga1.jpg")
-    cv2.imshow("Beluga Whale", beluga)
+    beluga = cv2.imread("images/beluga1.png")
+    # cv2.imshow("Beluga Whale", beluga)
     edged = imageManipulation(beluga)
     erodedFrame = morphManip(edged)
     c = contours(erodedFrame, beluga)
-    mask, maskedWhale = applyMask(c, beluga)
+    mask = applyMask(c, beluga)
 
-    plot_histogram(beluga, "Histogram for Masked Image", mask = mask)
+    hists = plot_histogram(beluga, "Histogram for Masked Image", mask = mask)
+
+    return hists
 
 def histofOImage():
-    blue()
-    #fin()
-    #humpback()
-    #beluga()
+    blueHists = blue()
+    finHists = fin()
+    humpbackHists = humpback()
+    belugaHists = beluga()
+    allHists = [blueHists, finHists, humpbackHists, belugaHists]
 
-def histofNImage():
+    return allHists
+
+def histofNImage(allHists):
     pass
+
 def main():
-    histofOImage()
-    histofNImage()
+    allHists = histofOImage()
+    histofNImage(allHists)
 
 
 main()
